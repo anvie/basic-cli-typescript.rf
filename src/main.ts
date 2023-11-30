d2baf1ba9075f4f02e7289c74614e227ccfbd680
Robin Syihab <obin.mf@gmail.com>
2023-11-30 12:59:14 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,19 @@
 
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
+const version = require('../package.json').version;
+
+console.log(`$name$ v${version}`);
 
 yargs(hideBin(process.argv))
-  .commandDir('commands')
+  .commandDir('commands', {
+    visit(commandModule) {
+      return commandModule;
+    },
+    extensions: ['js', 'ts']
+  })
   .strict()
   .alias({ h: 'help' })
   .argv;
+
+
